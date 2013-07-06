92db50393cf729c4081ba240d16999f99b50e314
Derek Scruggs <me@derekscruggs.com>
2013-07-05 19:46:33 -0600

    Added Phase model, domain id constant

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130705210620) do
+ActiveRecord::Schema.define(version: 20130705223421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "domains", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "phases", force: true do |t|
+    t.string   "name"
+    t.integer  "sort_order", limit: 2
+    t.integer  "domain_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
